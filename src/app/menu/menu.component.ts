24f5b9ab3e5c56179749c39d1df06e73eacffb18
max777888 <issamnader@gmail.com>
2017-08-18 20:34:15 +0300

    node server part2

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,6 +2,7 @@ import { Dish } from '../shared/dish';
 import {DISHES} from '../shared/dishes';
 import { DishService } from '../services/dish.service';
 import { Component, OnInit, Inject } from '@angular/core';
+ 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -11,12 +12,16 @@ import { Component, OnInit, Inject } from '@angular/core';
 export class MenuComponent implements OnInit {
   dishes : Dish[];
   selectedDish: Dish;
+   errMess: string;
       constructor(private dishService: DishService,
     @Inject('BaseURL') private BaseURL) { }
   ngOnInit() {
    // this.DishService.getDishes()
    //   .then(dishes => this.dishes = dishes);
-   this.dishService.getDishes().subscribe(dishes => this.dishes = dishes);
+ //  this.dishService.getDishes().subscribe(dishes => this.dishes = dishes);
+    this.dishService.getDishes()
+    .subscribe(dishes => this.dishes = dishes,
+      errmess => this.errMess = <any>errmess);
   }
 
   onSelect(dish:Dish){
--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -9,12 +9,39 @@ import { Http, Response } from '@angular/http';
 import { baseURL } from '../shared/baseurl';
 import { ProcessHTTPMsgService } from './process-httpmsg.service';
 import 'rxjs/add/operator/catch';
+
 @Injectable()
 export class DishService {
 
     constructor(private http: Http,
               private processHTTPMsgService: ProcessHTTPMsgService) { }
 
+getDishes(): Observable<Dish[]> {
+    return this.http.get(baseURL + 'dishes')
+                    .map(res => { return this.processHTTPMsgService.extractData(res); })
+                    .catch(error => { return this.processHTTPMsgService.handleError(error); });
+  }
+
+  getDish(id: number): Observable<Dish> {
+    return  this.http.get(baseURL + 'dishes/'+ id)
+                    .map(res => { return this.processHTTPMsgService.extractData(res); })
+                    .catch(error => { return this.processHTTPMsgService.handleError(error); });
+  }
+
+  getFeaturedDish(): Observable<Dish> {
+    return this.http.get(baseURL + 'dishes?featured=true')
+                    .map(res => { return this.processHTTPMsgService.extractData(res)[0]; })
+                    .catch(error => { return this.processHTTPMsgService.handleError(error); });
+  }
+
+  getDishIds(): Observable<number[]> {
+    return this.getDishes()
+      .map(dishes => { return dishes.map(dish => dish.id) })
+      .catch(error => { return error; } );
+  }
+
+
+/*
  getDishes(): Observable<Dish[]> {
     return this.http.get(baseURL + 'dishes')
                     .map(res => { return this.processHTTPMsgService.extractData(res); });
@@ -34,7 +61,7 @@ export class DishService {
     return this.getDishes()
       .map(dishes => { return dishes.map(dish => dish.id) });
   }
-  
+  */
 
 /*        
  getDishes(): Observable<Dish[]> {
